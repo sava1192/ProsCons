a47e0365324872a4d9f8169d5a00a1704c3a2a3c
AndriiZerkal <andrew.zerkal@gmail.com>
2015-08-03 16:21:41 +0300

    add css and update js and fix the html

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
 var el = document.getElementById('minus');
-el.addEventListener('click', myFunction);
-function myFunction() {
+el.addEventListener('click', addMinus);
+function addMinus() {
     var list = document.getElementById('cons');
     var li = document.createElement('LI'); 
-    li.innerHTML = document.getElementById('a').value;
+    li.innerHTML = document.getElementById('second_inp').value;
+    list.appendChild(li);
+};
+var el = document.getElementById('plus');
+el.addEventListener('click', addPlus);
+function addPlus() {
+    var list = document.getElementById('pros');
+    var li = document.createElement('LI'); 
+    li.innerHTML = document.getElementById('first_inp').value;
     list.appendChild(li);
 };
